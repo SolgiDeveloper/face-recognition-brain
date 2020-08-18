d1b407504768aad17c15036579ff8e3169a03360
SolgiDeveloper <mohammadsolgi96@gmail.com>
2020-08-18 21:29:29 +0430

    add APIs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation.js';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition.js';
 import Logo from './components/Logo/Logo.js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
 import Rank from './components/Rank/Rank.js';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 
 import './App.css';
+
+
+const app = new Clarifai.App({
+  apiKey: 'c7b11da0724642cc94b11933d788c063'
+ });
 const particlesOptions = {
   particles: {
     number:{
@@ -17,19 +24,49 @@ const particlesOptions = {
     }
   }
 }
-function App() {
-  return (
-    <div className="App">
-    <Particles className='particles'
-      params = {particlesOptions}
-    />
-      <Navigation/>
-      <Logo/>
-      <Rank/>
-      <ImageLinkForm/>
-      {/*<FaceRecognition/>*/}
-    </div>
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+      imageUrl: ''
+    }
+  }
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+  onButtonSubmit=() =>{
+    this.setState({imageUrl: this.state.input})
+    app.models.predict(
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input)
+      .then(
+      function(response) {
+        // do something with response
+        console.log(response.outputs[0].dara.regions[0].region_info.bounding_box);
+      },
+      function(err) {
+        // there was an error
+      }
   );
-}
+  }
+    render() {
+      return (
+        <div className="App">
+        <Particles className='particles'
+          params = {particlesOptions}
+        />
+          <Navigation/>
+          <Logo/>
+          <Rank/>
+          <ImageLinkForm
+           onInputChange= {this.onInputChange}
+           onButtonSubmit= {this.onButtonSubmit}
+          />
+          <FaceRecognition imageurl={this.state.imageUrl}/>
+        </div>
+      );
+    }
+  }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './ImageLinkForm.css';
-const ImageLinkForm = () => {
+const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
   return (
     <div >
       <p className='f3'>
@@ -8,8 +8,11 @@ const ImageLinkForm = () => {
       </p>
       <div className='center'>
       <div className=' center form pa4 br3 shadow-5'>
-        <input className='f4 pa2 w-70 center' type='text' />
-        <button className='w-30 grow f4 link ph3 pv2 dib black bg-light-green'>Detect</button>
+        <input className='f4 pa2 w-70 center' type='text' placeholder='Enter your image url!' onChange={onInputChange} />
+        <button
+         className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
+         onClick={onButtonSubmit}
+         >Detect</button>
       </div>
         
       </div>
