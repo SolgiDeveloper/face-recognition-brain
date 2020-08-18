573be74b767e95d2492e02274b221be61b7740fd
SolgiDeveloper <mohammadsolgi96@gmail.com>
2020-08-19 02:47:05 +0430

    complit front end

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import './FaceRecognition.css';
 
-const faceRecognition = ({imageUrl}) => {
+const faceRecognition = ({imageUrl, box}) => {
   return (
     <div className='center'>
     <div className='absolute mt2'>
-      <img alt='' src={imageUrl} width='500px' height='auto'/>
+      <img id='inputimage' alt='' src={imageUrl} width='500px' height='auto'/>
+      <div className='bounding-box' 
+      style={{
+        top: box.topRow,
+        right: box.rightCol,
+        bottom: box.bottomRow,
+        left: box.leftCol
+      }}>
+      </div>
     </div>
       
     </div>
